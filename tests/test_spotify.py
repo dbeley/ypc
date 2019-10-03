8fc835ba00c76d86cb67a1816039d523c30dc124
dbeley <6568955+dbeley@users.noreply.github.com>
2019-10-03 11:33:15 +0200

    Disable spotify tests for travis.

--- tests/test_spotify.py
+++ tests/test_spotify.py
@@ -1,17 +1,16 @@
 from ypc import spotify_utils
 import pandas as pd
-
-# import pytest
-# import os
+import pytest
+import os
 
 SPOTIFY_FILE = "tests/test_files/spotify_urls.txt"
 SPOTIFY_URL = "https://open.spotify.com/playlist/37i9dQZF1DX2sUQwD7tbmL"
 
 
-# @pytest.mark.skipif(
-#     "TRAVIS" in os.environ and os.environ["TRAVIS"] == "true",
-#     reason="doesn't work with Travis",
-# )
+@pytest.mark.skipif(
+    "TRAVIS" in os.environ and os.environ["TRAVIS"] == "true",
+    reason="doesn't work with Travis",
+)
 def test_get_spotify_playlist_tracks(sp):
     if not isinstance(
         spotify_utils.get_spotify_playlist_tracks(sp, None, SPOTIFY_URL), list
@@ -19,10 +18,10 @@ def test_get_spotify_playlist_tracks(sp):
         raise AssertionError()
 
 
-# @pytest.mark.skipif(
-#     "TRAVIS" in os.environ and os.environ["TRAVIS"] == "true",
-#     reason="doesn't work with Travis",
-# )
+@pytest.mark.skipif(
+    "TRAVIS" in os.environ and os.environ["TRAVIS"] == "true",
+    reason="doesn't work with Travis",
+)
 def test_get_spotify_playlists(sp):
     if not isinstance(
         spotify_utils.get_spotify_playlists(sp, [SPOTIFY_URL, SPOTIFY_URL]),
