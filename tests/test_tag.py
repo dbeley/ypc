77a6fa5d9d9b00145c871719b4d34286b348ea71
dbeley <6568955+dbeley@users.noreply.github.com>
2020-02-14 20:52:38 +0100

    Update tag extraction testing.

--- tests/test_tag.py
+++ tests/test_tag.py
@@ -2,12 +2,19 @@ from ypc import tag_utils
 
 
 def test_tag():
+    # first method : string matching
     metadata = tag_utils.get_metadata("Pinback - Loro")
     if not metadata.artist_name == "Pinback":
         raise AssertionError()
     if not metadata.track_name == "Loro":
         raise AssertionError()
-    if not metadata.primary_genre_name == "Pop":
+
+    # second method : itunes search
+    metadata = tag_utils.get_metadata("radiohead in limbo")
+    print(metadata)
+    if not metadata.artist_name == "Radiohead":
+        raise AssertionError()
+    if not metadata.track_name == "In Limbo":
         raise AssertionError()
 
 
